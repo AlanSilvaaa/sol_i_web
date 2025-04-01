a41f88b2d987336062e084cf564db86b56e8273e
Vinbu <vincent.14.damian@gmail.com>
2025-04-01 13:47:57 -0300

    Date and hour

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from datetime import datetime
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    current_time = datetime.now()
+    return {"Date and time": current_time}
